a8a37ea2140d9a0e16fee621f2366e349807499f
Phanasorn <you@example.com>
2021-11-19 01:39:31 +0700

    add forgot screen to Router

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:cpe_application/routing_constants.dart';
+import 'package:cpe_application/screens/forgot_password/forgot_password.dart';
 import 'package:cpe_application/screens/login/login.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,8 @@ Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case HomeRoute:
       return MaterialPageRoute(builder: (context) => Login());
+    case ForgotPasswordRoute:
+      return MaterialPageRoute(builder: (context) => ForgotPassword());
     default:
       return MaterialPageRoute(builder: (context) => Login());
   }
--- lib/routing_constants.dart
+++ lib/routing_constants.dart
@@ -1,2 +1,3 @@
 const String HomeRoute = 'Home';
 const String LoginRoute = 'Login';
+const String ForgotPasswordRoute = 'ForgotPassword';
