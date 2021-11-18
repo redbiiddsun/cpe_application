4ef910a150243d95f8f67c952cf2ce0f5b879aae
Phanasorn <you@example.com>
2021-11-18 21:39:59 +0700

    convert to stateful

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
