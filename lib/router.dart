244867f77e8e82014545f77d9024c451f18f416c
Phanasorn <you@example.com>
2021-12-10 05:44:00 +0700

    add notification route

--- lib/router.dart
+++ lib/router.dart
@@ -3,6 +3,7 @@ import 'package:cpe_alert/screens/class/class.dart';
 import 'package:cpe_alert/screens/forgot_password/forgot_password.dart';
 import 'package:cpe_alert/screens/login/login.dart';
 import 'package:cpe_alert/screens/main/main_screen.dart';
+import 'package:cpe_alert/screens/notification/notification.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
@@ -14,6 +15,8 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => Login());
     case ClassRoute:
       return MaterialPageRoute(builder: (context) => Class());
+    case NotificationRoute:
+      return MaterialPageRoute(builder: (context) => NotificationScreen());
     case ForgotPasswordRoute:
       return MaterialPageRoute(builder: (context) => ForgotPassword());
     default:
--- lib/routing_constants.dart
+++ lib/routing_constants.dart
@@ -1,5 +1,6 @@
 const String MainRoute = 'Main';
 const String HomeRoute = 'Home';
+const String NotificationRoute = 'Notification';
 const String LoginRoute = 'Login';
 const String ClassRoute = 'Class';
 const String ForgotPasswordRoute = 'ForgotPassword';
