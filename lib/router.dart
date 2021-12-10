e50276a4e72a7b4a7d707ff2f18bef9242ff21ee
Phanasorn <you@example.com>
2021-12-11 00:11:53 +0700

    add assignment to route

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,7 @@
 import 'package:cpe_alert/routing_constants.dart';
+import 'package:cpe_alert/screens/assignment/assignment.dart';
 import 'package:cpe_alert/screens/class/class.dart';
+import 'package:cpe_alert/screens/classmate/classmate.dart';
 import 'package:cpe_alert/screens/forgot_password/forgot_password.dart';
 import 'package:cpe_alert/screens/login/login.dart';
 import 'package:cpe_alert/screens/main/main_screen.dart';
@@ -17,6 +19,10 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => Class());
     case NotificationRoute:
       return MaterialPageRoute(builder: (context) => NotificationScreen());
+    case AssignmentRoute:
+      return MaterialPageRoute(builder: (context) => Assignment());
+    case ClassmateRoute:
+      return MaterialPageRoute(builder: (context) => Classmate());
     case ForgotPasswordRoute:
       return MaterialPageRoute(builder: (context) => ForgotPassword());
     default:
--- lib/routing_constants.dart
+++ lib/routing_constants.dart
@@ -1,6 +1,8 @@
 const String MainRoute = 'Main';
 const String HomeRoute = 'Home';
 const String NotificationRoute = 'Notification';
+const String ClassmateRoute = 'Classmate';
+const String AssignmentRoute = 'Assignment';
 const String LoginRoute = 'Login';
 const String ClassRoute = 'Class';
 const String ForgotPasswordRoute = 'ForgotPassword';
