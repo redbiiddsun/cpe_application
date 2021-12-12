9d71505b2bd41462393d8fa53571ab935ebcc454
Phanasorn <you@example.com>
2021-12-13 01:48:16 +0700

    update init firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:cpe_alert/routing_constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cpe_alert/router.dart' as router;
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  /*FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });*/
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
