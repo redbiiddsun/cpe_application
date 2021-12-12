e8241622333300457b552397c95383bcedd4c921
Phanasorn <you@example.com>
2021-12-13 01:47:28 +0700

    update classcard to de dynamic and reuse code

--- lib/screens/class/component/classcard.dart
+++ lib/screens/class/component/classcard.dart
@@ -2,8 +2,17 @@ import 'package:cpe_alert/screen_information.dart';
 import 'package:flutter/material.dart';
 
 class ClassCard extends StatelessWidget {
+  final String classNo;
+  final String classdesicribe;
+  final String time1;
+  final String time2;
+
   const ClassCard({
     Key? key,
+    required this.classNo,
+    required this.classdesicribe,
+    required this.time1,
+    this.time2 = "",
   }) : super(key: key);
 
   @override
@@ -18,7 +27,7 @@ class ClassCard extends StatelessWidget {
           top: ScreenInformation.screenHeight! * 0.0455,
           left: ScreenInformation.screenWidth! * 0.0533),
       decoration: BoxDecoration(
-        color: Color(0xFFD3E1FF),
+        color: const Color(0xFFD3E1FF),
         borderRadius: BorderRadius.circular(20),
       ),
       child: Row(
@@ -27,38 +36,37 @@ class ClassCard extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                "CPE123",
-                style: TextStyle(
+                classNo,
+                style: const TextStyle(
                   fontFamily: "Montserrat",
                   fontWeight: FontWeight.w600,
                   fontSize: 20,
                   color: Color(0xFF000000),
                 ),
               ),
-              SizedBox(height: 5),
+              const SizedBox(height: 5),
               Text(
-                "USER EXPERIENCE/USER INTERFACE",
-                style: TextStyle(
+                classdesicribe,
+                style: const TextStyle(
                   fontFamily: "Montserrat",
                   //fontWeight: FontWeight.w600,
                   fontSize: 12,
                   color: Color(0xFF000000),
                 ),
               ),
-              SizedBox(height: 5),
+              const SizedBox(height: 5),
               Text(
-                "Wednesday, 10:30-12:30",
-                style: TextStyle(
+                time1,
+                style: const TextStyle(
                   fontFamily: "Montserrat",
-                  //fontWeight: FontWeight.w600,
                   fontSize: 12,
                   color: Color(0xFF000000),
                 ),
               ),
-              SizedBox(height: 5),
+              const SizedBox(height: 5),
               Text(
-                "Wednesday, 10:30-12:30",
-                style: TextStyle(
+                time2,
+                style: const TextStyle(
                   fontFamily: "Montserrat",
                   //fontWeight: FontWeight.w600,
                   fontSize: 12,
