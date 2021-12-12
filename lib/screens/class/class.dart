58594ead6c5f49527403776f2fe8593cc1865682
Phanasorn <you@example.com>
2021-12-13 01:46:55 +0700

    add offline detail & Listviewbuilder

--- lib/screens/class/class.dart
+++ lib/screens/class/class.dart
@@ -2,6 +2,7 @@ import 'package:cpe_alert/screen_information.dart';
 import 'package:cpe_alert/screens/class/component/classappbar.dart';
 import 'package:cpe_alert/screens/class/component/classcard.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Class extends StatefulWidget {
   const Class({Key? key}) : super(key: key);
@@ -11,6 +12,54 @@ class Class extends StatefulWidget {
 }
 
 class _ClassState extends State<Class> {
+  final cpeClass = [
+    [
+      "CPE100",
+      "COMPUTER PROGRAMMING FORENGINEERS",
+      "Tuesday, 8:30-10:30",
+      "Tuesday, 13:30-15:30",
+      "https://us06web.zoom.us/j/8672073434?pwd=MnNMWXppK2YzTFpHdjFWc0JOdENCdz09"
+    ],
+    [
+      "CPE101",
+      "Computer Programming for Engineers",
+      "Friday, 15:30-17:30",
+      "",
+      "https://www.facebook.com/groups/4443556728990006"
+    ],
+    [
+      "CPE111",
+      "Discrete Mathematics for Computer Engineers",
+      "Thursday, 9:30-12:30",
+      "Friday, 10:30-12:30",
+      "https://kmutt-ac-th.zoom.us/j/92218683317"
+    ],
+    [
+      "CPE123",
+      "USER EXPERIENCE/USER INTERFACE",
+      "Wednesday, 10:30-12:30",
+      "Thursday, 10:30-12:30",
+      "https://us06web.zoom.us/j/8672073434?pwd=MnNMWXppK2YzTFpHdjFWc0JOdENCdz09"
+    ],
+    [
+      "MTH101",
+      "Mathematics",
+      "Monday, 8:30-10:30",
+      "Tuesday, 10:30-12:30",
+      "https://kmutt-ac-th.zoom.us/j/92218683317"
+    ],
+    [
+      "LNG221",
+      "ACADEMIC ENGLISH IN INTERNATIONAL CONTEXTS",
+      "Thursday, 13:30-15:30",
+      "https://line.me/R/ti/g/WkHQEkIA1R"
+    ],
+  ];
+
+  void _launchURL(String _url) async {
+    if (!await launch(_url)) throw 'Could not launch $_url';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +68,7 @@ class _ClassState extends State<Class> {
         backgroundColor: Colors.white,
         centerTitle: true,
         elevation: 0,
-        title: Text(
+        title: const Text(
           "Class",
           style: TextStyle(
             fontFamily: "Montserrat",
@@ -29,7 +78,22 @@ class _ClassState extends State<Class> {
           ),
         ),
       ),
-      body: ClassCard(),
+      body: ListView.builder(
+          itemCount: 6,
+          itemBuilder: (BuildContext context, int index) {
+            //String classNo = cpeClass[index][1];
+            return GestureDetector(
+              onTap: () {
+                _launchURL(cpeClass[index][4]);
+              },
+              child: ClassCard(
+                classNo: cpeClass[index][0],
+                classdesicribe: cpeClass[index][1],
+                time1: cpeClass[index][2],
+                time2: cpeClass[index][3],
+              ),
+            );
+          }),
     );
   }
 }
