d267a6f41065f8756a68f46bf50e1a9d7c47bf53
Phanasorn <you@example.com>
2021-12-14 12:12:10 +0700

    update function check user login or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,35 @@
 import 'package:cpe_alert/routing_constants.dart';
+import 'package:cpe_alert/screens/main/main_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cpe_alert/router.dart' as router;
 
-void main() async {
+Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  /*FirebaseAuth.instance.authStateChanges().listen((User? user) {
-    if (user == null) {
-      print('User is currently signed out!');
-    } else {
-      print('User is signed in!');
-    }
-  });*/
-
-  runApp(MyApp());
+  await Firebase.initializeApp().then((value) async {
+    await FirebaseAuth.instance.authStateChanges().listen((event) {
+      if (event != null) {
+        //if user login
+        // ignore: unused_local_variable
+        String uid = event.uid;
+        runApp(MyApp(
+          intiroute: MainRoute,
+        ));
+      } else {
+        //if user logout
+        runApp(MyApp(
+          intiroute: LoginRoute,
+        ));
+      }
+    });
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final String intiroute;
+
+  const MyApp({Key? key, required this.intiroute}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +40,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       onGenerateRoute: router.generateRoute,
-      initialRoute: LoginRoute,
+      initialRoute: intiroute,
     );
   }
 }
